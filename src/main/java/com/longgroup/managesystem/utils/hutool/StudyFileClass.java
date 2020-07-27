7425cb7386b230f9d1d2800d283f774db05d9a94
qlh <qlh2561808384@163.com>
2020-07-27 22:35:55 +0800

    ''

---
+++ src/main/java/com/longgroup/managesystem/utils/hutool/StudyFileClass.java
@@ -0,0 +1,14 @@
+package com.longgroup.managesystem.utils.hutool;
+
+import javax.crypto.spec.PSource;
+import java.io.File;
+
+/**
+ *  File 类源码学习
+ */
+public class StudyFileClass {
+    public static void main(String[] args) {
+        File file = new File("C:\\Users\\qlh\\Desktop\\JAVA核心知识点整理.pdf");
+        System.out.println(file);
+    }
+}
